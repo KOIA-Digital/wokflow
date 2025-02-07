3b7e7e773ff8c2939f47425bcd95505ab967e822
Kopec-Srandy <96027751+Kopec-Srandy@users.noreply.github.com>
2025-02-07 10:01:24 +0100

    Added github link to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,13 @@
 "use client"
 
-import { FaTelegram, FaTwitter } from "react-icons/fa6";
+import { FaGithub, FaTelegram, FaTwitter } from "react-icons/fa6";
 
 export default function Footer() {
     return <div className="flex flex-col gap-2 p-10 mt-auto items-center">
     <div className="flex gap-2">
         <a href="https://twitter.com/wok_flow" target="_blank" rel="noopener noreferrer"><FaTwitter size={30}/></a>
         <a href="https://t.me/wokflow" target="_blank" rel="noopener noreferrer"><FaTelegram size={30}/></a>
+        <a href="https://github.com/KOIA-Digital/wokflow" target="_blank" rel="noopener noreferrer"><FaGithub size={30}/></a>
     </div>
     <div>All rights reserved to KOIA s.r.o.</div>
 </div>
